ee9c42a6e4446481ddb335fe2693dabba9c8ac93
David Zeindler <dave.zeindler@gmail.com>
2024-12-05 00:04:13 +0100

    first commit with terminal component and according client terminal service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
-import { InputTextModule } from 'primeng/inputtext';
-import { ButtonModule } from 'primeng/button';
-import { MessageModule } from 'primeng/message';
-import { CommonModule } from '@angular/common';
+import {Component} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {RouterOutlet} from '@angular/router';
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+import {MessageModule} from 'primeng/message';
+import {CommonModule} from '@angular/common';
+import {TerminalComponent} from './terminal/terminal.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, InputTextModule, ButtonModule, MessageModule, FormsModule],
+  imports: [CommonModule, RouterOutlet, InputTextModule, ButtonModule, MessageModule, FormsModule, TerminalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/terminal/terminal.component.ts
@@ -0,0 +1,35 @@
+import {Component, OnDestroy} from '@angular/core';
+import {Terminal, TerminalService} from 'primeng/terminal';
+import {Subscription} from 'rxjs';
+import {TerminalCommandService} from '../service/terminal-command.service';
+
+@Component({
+  selector: 'app-terminal',
+  standalone: true,
+  templateUrl: './terminal.component.html',
+  styleUrl: './terminal.component.scss',
+  imports: [
+    Terminal
+  ],
+  providers: [
+    TerminalService
+  ]
+
+})
+export class TerminalComponent implements OnDestroy {
+  subscription: Subscription;
+
+
+  constructor(private terminalService: TerminalService, private terminalCpmmandService: TerminalCommandService) {
+    this.subscription = this.terminalService.commandHandler.subscribe((command: string) => {
+      this.terminalService.sendResponse(terminalCpmmandService.getResponse(command));
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+
+}
